d04c712e8181c3b097e6ee455dcee591d894ae72
Mariannemyrvold <marmyr6@hotmail.com>
2023-02-27 12:56:36 +0100

    Ryddet
    Jennifer ryddet opp

--- arbeidskrav/src/App.js
+++ arbeidskrav/src/App.js
@@ -2,21 +2,13 @@ import Article from "./components/Article";
 import Nav from "./components/Nav";
 import Header from  "./components/Header";
 
-
-
 function App() {
   return (
     <section>
-
-  <Header />
-    
-   <Nav />
-  
-   <Article />
- 
-   
-   </section>
-
+      <Header />
+      <Nav />
+      <Article />
+    </section>
   );
 }
 
--- arbeidskrav/src/components/Article.js
+++ arbeidskrav/src/components/Article.js
@@ -1,4 +1,3 @@
-
 export default function Article () {
 
   /* resources.map((resources) => {
