f6f7e6d87e6168dd21639dd701fdba7dc926cd18
jennifergri <jennifergr@hotmail.no>
2023-03-01 11:00:05 +0100

    retter opp feil og liker til css

--- arbeidskrav/src/App.js
+++ arbeidskrav/src/App.js
@@ -2,7 +2,8 @@ import Article from "./components/Article";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Layout from "./components/Layout";
-import Header from "./components/Header";
+import './App.css';
+import './css/main.css';
 
 function App({menyItems, index, title, url}) {
   return (
