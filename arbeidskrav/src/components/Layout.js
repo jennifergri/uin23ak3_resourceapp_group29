dc52323362b5429bb1c9bbb5de5ef698b8f7e34d
jennifergri <jennifergr@hotmail.no>
2023-03-01 10:52:05 +0100

    riktig oppsett i layout

--- arbeidskrav/src/components/Layout.js
+++ arbeidskrav/src/components/Layout.js
@@ -1,19 +1,15 @@
-import { Link, Outlet } from "react-router-dom";
-
 export default function Layout() {
     return (
         <div id="container">
         <header>
-            <Link to="/">
-                <h1>{}</h1>
-            </Link>
+            <h1>Ressursarkiv</h1>
         </header>
-            <main>
-                <Outlet/>
-            </main>
-        <footer>
 
-        </footer>
+        <main>
+            <article>
+
+            </article>
+        </main>
       </div>
     )
 }
