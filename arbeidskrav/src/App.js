890c58c767abc701981de4c20258a057b2a04f7d
jennifergri <jennifergr@hotmail.no>
2023-03-01 10:56:47 +0100

    fjernet header.js for den er i layout

--- arbeidskrav/src/App.js
+++ arbeidskrav/src/App.js
@@ -1,6 +1,4 @@
 import Article from "./components/Article";
-import Header from  "./components/Header";
-import '../src/css';
 import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Layout from "./components/Layout";
@@ -8,10 +6,9 @@ import Layout from "./components/Layout";
 function App({menyItems, index, title, url}) {
   return (
     <Layout>
-      <Header />
-        <Nav />
-        <Main />
-        <Article />
+      <Nav />
+      <Main />
+      <Article />
     </Layout>
   );
 }
