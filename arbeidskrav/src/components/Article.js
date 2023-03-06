0c4001540050b4b0b924fc5b751cdb83bc022424
HenrietteH <Henriettehestsveen@gmail.com>
2023-03-06 14:39:39 +0100

    Lagd ny branch, med noen test-artikkel kode

--- arbeidskrav/src/components/Article.js
+++ arbeidskrav/src/components/Article.js
@@ -1,20 +1,23 @@
-export default function Article({ data }) {
+import resources from "../resources/ressurser"
+
+export default function Article() {
   //Bruker data prop, for å få tak på innholdet i resources-arrayen. 
     return ( 
-<></>
 //Bruker map funksjonen, og kaller på data arrayen og til objektene (title, url og category) inni resources arrayen. Ber at artiklene skal inneholde listepunkter, og url. Sender props fra resources arrayen. 
-      /*{data.map(({ title, url, category }, index) => (
+      resources.map(({ title, url, category }, index) => (
       <article key={index} className="innhold">
             <h3>{category}</h3>
               <ul>
-                <li key={title}>{title}<li/>
-                <a key={url} href={url}/>
+                <li key={index}>
+                  {title}
+                  <a key={url} href={url}>
                 {url}
                 </a>
+                </li>
               </ul>
         </article> 
-      ))}
-        </>*/
-        );  
-        } 
+      ))
+      )
+    }  
+        
       
