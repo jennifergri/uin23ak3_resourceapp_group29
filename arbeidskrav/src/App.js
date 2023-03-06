1cbc3a8d4436b785837f210ea5fe271f658ed2f0
HenrietteH <Henriettehestsveen@gmail.com>
2023-03-06 14:19:04 +0100

    La inn noen kommentarer til senere

--- arbeidskrav/src/App.js
+++ arbeidskrav/src/App.js
@@ -2,6 +2,9 @@ import Article from "./components/Article";
 import Nav from "./components/Nav";
 // import Main from "./components/Main";
 import Layout from "./components/Layout";
+//importere resourses?
+//Importere css.stilarket?
+//I App.js filen er alt som skal rendres ut på nettsiden :) 
 
 function App() {
   return (
